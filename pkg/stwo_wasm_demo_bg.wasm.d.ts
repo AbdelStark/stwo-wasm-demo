a0c7cdc7f2f7fa00771ae7c9aec85b2589d5eb0d
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2024-06-27 18:15:17 +0200

    Manual update of gh-pages content

---
+++ pkg/stwo_wasm_demo_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_fibonacciresult_free(a: number): void;
+export function fibonacciresult_success(a: number): number;
+export function fibonacciresult_message(a: number, b: number): void;
+export function run_fibonacci_example(a: number, b: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
